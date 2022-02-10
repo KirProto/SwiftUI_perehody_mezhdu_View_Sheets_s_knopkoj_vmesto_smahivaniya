145a572a4b61e78ff2fd7820de75c2c397e169e8
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 11:33:59 +0500

    Initial Commit

---
+++ Yan_SwiftUI_perehody_mezhdu_View_Sheets_s_knopkoj_vmesto_smahivaniya/Yan_SwiftUI_perehody_mezhdu_View_Sheets_s_knopkoj_vmesto_smahivaniyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_perehody_mezhdu_View_Sheets_s_knopkoj_vmesto_smahivaniyaApp.swift
+//  Yan_SwiftUI_perehody_mezhdu_View_Sheets_s_knopkoj_vmesto_smahivaniya
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_perehody_mezhdu_View_Sheets_s_knopkoj_vmesto_smahivaniyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
